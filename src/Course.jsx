eae0ea35ce7a92b3fc0aa68825080803952e9037
dachi papashvili <115042460+Dachi-Papashvili88@users.noreply.github.com>
2024-06-13 07:41:23 +0000

    render total

--- src/Course.jsx
+++ src/Course.jsx
@@ -11,14 +11,24 @@ const Content = ({ parts }) => (
     <Part part={parts[0]} />
     <Part part={parts[1]} />
     <Part part={parts[2]} />
+    <Part part={parts[3]} />
   </>
 );
+
+const Total = ({ sum }) => <p>Total of {sum} exercises</p>;
 
 const Course = ({ course }) => {
   return (
     <>
       <Header course={course} />
       <Content parts={course.parts} />
+      <Total
+        sum={
+          course.parts[0].exercises +
+          course.parts[1].exercises +
+          course.parts[2].exercises
+        }
+      />
     </>
   );
 };
